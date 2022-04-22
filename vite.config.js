19f11bb53e56240d3af34c4e092a9b41c272d577
hanbowen <hanbowen@lucigo.cn>
2022-04-22 20:06:40 +0800

    add aframe demo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 // vite.config.js
+const { resolve } = require('path')
 import { defineConfig } from 'vite'
 export default defineConfig({
     // config options
     root: "src",
     build:{
+        rollupOptions: {
+            input: {
+              main: resolve(__dirname, 'src/index.html'),
+              aframe: resolve(__dirname, 'src/aframe/index.html')
+            }
+          },
         outDir: '../dist',
     },
   })
